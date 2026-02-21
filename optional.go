1d712ab57e4e5def1e05cab31641c98d1e8dbcf3
AlexSkilled <olegthander@yandex.ru>
2026-02-21 20:42:27 +0300

    [master] Optional values

--- optional.go
+++ optional.go
@@ -1,5 +1,10 @@
 package toolbox
 
+import (
+	"bytes"
+	"encoding/json"
+)
+
 type Optional[T any] struct {
 	Value T
 	Valid bool
@@ -10,4 +15,27 @@ func NewOptional[T any](value T) Optional[T] {
 		Value: value,
 		Valid: true,
 	}
+}
+
+func (o *Optional[T]) UnmarshalJSON(v []byte) error {
+	if bytes.Equal(bytes.TrimSpace(v), []byte("null")) {
+		var zero T
+		o.Value = zero
+		o.Valid = false
+		return nil
+	}
+
+	err := json.Unmarshal(v, &o.Value)
+
+	o.Valid = err == nil
+
+	return err
+}
+
+func (o Optional[T]) MarshalJSON() ([]byte, error) {
+	if !o.Valid {
+		return []byte("null"), nil
+	}
+
+	return json.Marshal(o.Value)
 }
